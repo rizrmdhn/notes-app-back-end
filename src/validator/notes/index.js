4847d441721cb6f04d212075ec15dc0691f95e4f
rizrmdhn <rizrmdhn.work@gmail.com>
2022-12-18 09:28:51 +0800

    postgres database implemented

---
+++ src/validator/notes/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+const InvariantError = require("../../exceptions/InvariantError");
+const { NotePayloadSchema } = require("./schema")
+
+
+const NotesValidator = {
+    validateNotePayload: (payload) => {
+        const validationResult = NotePayloadSchema.validate(payload);
+        if (validationResult.error) {
+            throw new InvariantError(validationResult.error.message);
+        }
+    },
+};
+
+module.exports = NotesValidator;
